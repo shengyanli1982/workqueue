9645751a352874f4c42e13c097bbf2aed20ef26f
shengyanli1982 <shengyanlee36@gmail.com>
2024-05-22 21:39:09 +0800

    Optimize test and performance content

--- internal/container/list/node_test.go
+++ internal/container/list/node_test.go
@@ -73,33 +73,3 @@ func TestNodePool_PutAndGet(t *testing.T) {
 	assert.Nil(t, node.Prev)
 	assert.Equal(t, int64(0), node.Index)
 }
-
-func BenchmarkNodePool_Get(b *testing.B) {
-	pool := NewNodePool()
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		pool.Get()
-	}
-}
-
-func BenchmarkNodePool_Put(b *testing.B) {
-	pool := NewNodePool()
-	node := NewNode()
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		pool.Put(node)
-	}
-}
-
-func BenchmarkNodePool_PutAndGet(b *testing.B) {
-	pool := NewNodePool()
-	node := pool.Get()
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		pool.Put(node)
-		node = pool.Get()
-	}
-}
